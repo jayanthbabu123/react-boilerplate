b8ca04ca69525eab8778b061b6bfe76ff837f505
jayanth <jsomineni@gmail.com>
2019-03-24 10:19:37 +0530

    eslint config changes

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,27 +1,32 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux';
-import * as Actions from '../../Store/Actions/index';
-import { MainDecorator } from '../../HOC/MainDecorator';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as Actions from "../../Store/Actions/index";
+import { MainDecorator } from "../../HOC/MainDecorator";
+import PropTypes from "prop-types";
 class Home extends Component {
   state = {
     name: "home"
+  };
+  static get propTypes() {
+    return {
+      getPostsData: PropTypes.func
+    };
   }
   componentDidMount() {
     this.props.getPostsData();
   }
   render() {
-    return (
-      <div>
-        Home
-      </div>
-    )
+    return <div>Home</div>;
   }
 }
 const mapStateToProps = state => ({
   posts: state.posts.postItems
-})
+});
 const mapDispatchToProps = dispatch => ({
   getPostsData: () => dispatch(Actions.getPosts())
-})
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(MainDecorator(Home, {name:"Home"}));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MainDecorator(Home, { name: "Home" }));
